041f593e18b0bfda9fb81ed4314e5551e82f0201
swathi-swa <36359971+swathi-swa@users.noreply.github.com>
2019-01-24 18:28:18 +0530

    Add files via upload

---
+++ ArrayList.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+class Arraylist
+{
+public static void main(String[] args)
+{
+Scanner in=new scanner(ystem.in);
+ArrayList<Integer> ar=new ArrayList<Integer>();
+ar.add(2);
+ar.add(5);
+ar.add(1);
+ar.add(124);
+System.out.println(ar);
+}
+}
+
