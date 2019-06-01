6fe16a95fad9aa069b24e1abf5c24fbb95febfae
swathi-swa <36359971+swathi-swa@users.noreply.github.com>
2019-06-01 14:59:51 +0530

    Create ass.py

---
+++ ass.py
@@ -0,0 +1,337 @@
+pp1
+
+Trigonometric Functions implementations
+1. Write a program to find the sine of x radians.
+2. Write a program to find the cosine of x radians.
+3. Write a program to find the tangent of x radians.
+4. Write a program to find the arc sine of x radians.
+5. Write a program to find the arc cosine of x radians.
+6. Write a program to find the arc tangent of x radians.
+7. Write a program to Convert angle from radians to degrees
+8. Write a program to Convert angle x from degrees to radians.
+9. Write a program to print the value pi
+10. Write a program to print the value of e
+
+-----------------------------------------------------------------------------------
+pp2
+
+	
+Due: May 18
+Random method implementation
+1. Write a program to choose random item from a list, tuple, or string.
+2. Write a program to randomly select element from range(start, stop, step)
+3. Write a program to randomly select element from range 0 - 1 with floating point value
+4. Write a program to randomizes the items of a list in place
+5. Write a program to randomly select element from specified range with floating point value
+
+----------------------------------------------------------------------------------------------------
+
+pp3
+
+Program on Date time functions
+1. Write a program to execute the following method and verify the output
+
+time.altzone 
+time.asctime([tupletime]) 
+time.clock( ) 
+time.ctime() 
+time.gmtime() 
+time.localtime() 
+time.mktime(tupletime) 
+time.sleep(secs) 
+time.strftime(fmt[,tupletime]) 
+time.strptime(str,fmt = '%a %b %d %H:%M:%S %Y') 
+time.time( ) 
+time.tzset()
+
+----------------------------------------------------------------------------------------------------------
+
+pp4
+
+Calender Functions
+Write the program to execute the following methods and verify the output
+
+calendar(year,w = 2,l = 1,c = 6)
+firstweekday( )
+isleap(year)
+leapdays(y1,y2)
+month(year,month,w = 2,l = 1)
+monthcalendar(year,month) 
+monthrange(year,month)
+prcal(year,w = 2,l = 1,c = 6)
+prmonth(year,month,w = 2,l = 1)
+setfirstweekday(weekday)
+timegm(tupletime)
+weekday(year,month,day)
+
+-------------------------------------------------------------------------------------------------------------
+
+pp5
+
+1. Program to print the Factors of a Number N
+2. Program to check whether given Integer is Prime or Not
+3. Program to print the Prime numbers between two intervals
+4. Program to check whether the given digit is occurred in a Number
+5. Program to count the occurrence of digit
+6. Program to find the first digit of a number
+7. Program to print all the numbers which are less than given key element from a given list.
+8. Program to find Second Largest element in the list
+9. Program to find Second smallest element in the list
+10. Program to delete an element in an list
+11. Program to Reverse the Elements in list
+12. Count the occurrences of given Character in a String
+13. Covert Lowercase characters to Uppercase Characters
+14. Covert Uppercase characters to Lowercase Characters
+15. Count the number of vowels in given string
+
+
+-------------------------------------------------------------------------------------------------------------------------
+
+pp6
+
+Program to implement the Predefined exception
+1. Write a program to implement the following predefined exception
+1.1 Exception 
+1.2 StopIteration 
+1.3 SystemExit 
+1.4 StandardError 
+1.5 ArithmeticError 
+1.6 OverflowError 
+1.7 FloatingPointError 
+1.8 ZeroDivisonError 
+1.9 AssertionError 
+1.10 AttributeError 
+1.11 ImportError
+1.12 KeyboardInterrupt
+1.13 LookupError
+1.14 IndexError
+1.15 KeyError
+1.16 NameError
+1.17 UnboundLocalError
+1.18 EnvironmentError
+1.19 SyntaxError
+1.20 IndentationError
+1.21 TypeError
+1.22 ValueError
+1.23 NotImplementedError
+
+-----------------------------------------------------------------------------------------
+
+pp7
+
+Exception Sample Program
+1. Write a simple program with try, except, else and finally keyword implementation.
+2. Write a simple program with try, raise, except, else and finally keyword implementation.
+3. Write a simple program with assert keyword implementation.
+4. Write a simple program with user defined exception and call the user defined exception .
+
+--------------------------------------------------------------------------------------------------
+
+pp8
+
+Due: May 22
+Program for Practice
+1. Program to check whether given Integer is Perfect Square or not
+2. Program to reverse the digits of an number
+3. Program to reverse the first half of array elements
+4. Program to reverse the second half of array elements
+5. Program to Sort the Elements in ascending order
+6. Program to Sort the Elements in descending order
+7. Program to sort half of the Array elements in ascending order and next half in descending order
+8. Program to Merging two arrays
+9. Find the location of given sub string in a String
+10. Count the occurrences of given substring in a String
+
+-------------------------------------------------------------------------------------
+
+pp9
+
+File Operation
+1. Write a program to check the current position of the cursor.
+2. Write a program to set the cursor position and read the data from that.
+
+--------------------------------------------------------------------------------------
+
+pp10
+
+	
+Due: May 23
+Operating System based Operation
+1. Write the program to change the file name.
+2. Write the program to remove the file.
+3. Write the program to create the new directory
+4. Write the program to delete the directory
+5. Write the program to change the current working directory
+6. Write the program to get the current working directory
+
+
+----------------------------------------------------------------------------------------
+
+pp11
+
+1. Program to Print the Perfect squares between two intervals
+2. Program to Count the digits in the given number.
+3. Program to calculate sum of digits.
+4. Program to replace every element with the greatest element on right side 
+5. Program to perform circular Array Rotation
+6. Program to add two matrix
+7. Reverse the given String
+8. Check the given string is Palindrome
+
+------------------------------------------------------------------------------------------
+pp12
+
+1. Program to check whether given Integer is whether integer is power of 2 or not
+2. Program to find GCD of given numbers
+3. Program to find LCM of given numbers
+4. Program to calculate product of digits.
+5. Program to check whether given Integer is Palindrome or Not
+6. Program to check whether given Integer is Armstrong or Not
+7. Program to multiply two matrix
+8. Program to transpose the Matrix
+9. Remove the spaces in given String
+10. Remove the Vowels in given String
+
+--------------------------------------------------------------------------------------------------
+
+pp13
+
+1. If i=5
+* * * * *
+* * * * * 
+* * * * * 
+* * * * * 
+* * * * * 
+2. If 1=3
+*
+* *
+* * *
+If i = 5
+*
+* *
+* * *
+* * * * 
+* * * * *
+3. If i = 3 (Note for this program '-' should be replaced by empty space)
+-	-	*
+-	*	*
+*	*	*
+If i=5
+-	-	-	-	*
+-	-	-	*	*
+-	-	*	*	*
+-	*	*	*	*
+*	*	*	*	*
+
+------------------------------------------------------------------------------------
+
+pp14
+
+1. If i=3
+* * *
+* *
+*
+If i = 5
+* * * * *
+* * * *
+* * *
+* *
+*
+2. If i = 3 (Note for this program '-' should be replaced by empty space)
+*	*	*
+-	*	*
+-	-	*
+
+If i=5
+*	*	*	*	*
+-	*	*	*	*
+-	-	*	*	*
+-	-	-	*	*
+-	-	-	-	*
+
+-------------------------------------------------------------------------------------
+
+pp15
+
+1. Write a program with the following structure
+1.1 Class -> Calc
+1.2 Method -> add
+1.3 Method -> sub
+1.4 Method -> mul
+1.5 Method -> div
+Receive two parameter for processing in each method and display the result.
+
+
+2. Write a program with the following structure
+2.1 Class -> Calc
+2.2 Constructor which will assign the value for 2 variable a and b
+2.3 Method -> add
+2.4 Method -> sub
+2.5 Method -> mul
+2.6 Method -> div
+Consider the instance variable for processing in each method and display the result.
+
+
+3. Write a program with the following structure
+3.1 Class -> Calc
+3.2 Two variable a,b should be declared as static
+3.3 Static Method -> 'set' (which will assign the value for 2 variable a and b)
+3.4 Static Method -> add
+3.5 Static Method -> sub
+3.6 Static Method -> mul
+3.7 Static Method -> div
+Consider the Static variable for processing in each method and display the result. 
+Call all the methods with out creating object for the class. (As all are static)
+
+--------------------------------------------------------------------------------------------------------
+
+pp16
+	
+Due: May 29
+Class and Object Basics
+1. Create a empty class.
+2. Perform the following operation for the class
+2.1 Create a class with variables and methods of public category. 
+2.2 Create Object for the class.
+2.3 Access the class variables and methods with the created object
+3. Perform the following operation for the class
+3.1 Create a class 'Sample' with variables (x,y,z) and assign the value to the variables
+3.2 Write a methods (display() to display the value of x,y,z) of public category.
+3.3 Create the object for the class Sample and call the display method
+3.4 With the object access the variables x,y and z outside the class.
+4. Perform the following operation for the class
+4.1 Create a class 'Sample' with variables (x,y,z of private category) and assign the value to the variables
+4.2 Write a methods (display() to display the value of x,y,z) of public category.
+4.3 Create the object for the class Sample and call the display method
+4.4 With the object access the variables x,y and z outside the class.
+5. Perform the following operation for the class
+5.1 Create a class 'Sample' with variables (x,y,z of private category) and assign the value to the variables
+5.2 Write a methods (display() to display the value of x,y,z) of private category.
+5.3 Create the object for the class Sample and call the display method
+5.4 With the object access the variables x,y and z outside the class
+6. Perform the following operation for the class
+6.1 Create a class 'Sample' with variables (x,y,z of private category) and assign the value to the variables
+6.2 Write a methods (display() to display the value of x,y,z) of private category.
+6.3 Write a method (calldisplay() to call the display method) of general category.
+6.4 Create the object for the class Sample and call the calldisplay method
+
+-----------------------------------------------------------------------------------------------------------------
+
+pp17
+
+	
+Due: May 29
+Constructor Example
+1. Write a program with a class name, constructor(to initialize the instance value by receiving data from user), and method(to display the value).
+2. Write a program with a class name, constructor(to initialize the instance value by receiving data as parameters), and method(to display the value).
+
+
+------------------------------------------------------------------------------------------------------------------------------
+
+pp18
+
+	
+Due: May 29
+Instance of Object example program
+1. Write a program to declare the variables for the particular object and access the variables.
+2. Write a program to declare the variables inside the method and access the variables.
